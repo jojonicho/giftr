fcd037da6685ae3d3b2d2f0e087fc7b0d3a62fca
Jonathan Nicholas <jojonicho181@gmail.com>
2020-05-23 23:54:57 +0700

    :art: dotenv

--- src/utils/useFetch.ts
+++ src/utils/useFetch.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { MyResponse, TenorData } from "./types";
+import "dotenv";
 
 export const useFetch = (url: string) => {
   const [state, setState] = useState<{
